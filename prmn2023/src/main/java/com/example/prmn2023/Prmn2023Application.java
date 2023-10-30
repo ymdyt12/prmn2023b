48629eb00ade495dbfe365039a521b8553f203f9
ymdyt12 <b2222480@photon.chitose.ac.jp>
2023-10-30 15:39:16 +0900

    プロジェクト作成

---
+++ prmn2023/src/main/java/com/example/prmn2023/Prmn2023Application.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2023Application.class, args);
+    }
+
+}
---
+++ prmn2023/src/test/java/com/example/prmn2023/Prmn2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prmn2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
